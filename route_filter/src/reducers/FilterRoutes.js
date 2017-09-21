cb1871ee6e41cf9a38a7615c8a96ebd860a7edaf
OlgaGulenko <olga.gulenko@hotmail.com>
2017-09-21 15:31:18 +0300

    all

--- route_filter/src/reducers/FilterRoutes.js
+++
@@ -1,8 +0,0 @@
-const initialState = '';
-
-export default function FilterRoutes(state = initialState, action) {
-  if (action.type === 'FIND_Route') {
-    return action.payload;
-  }
-  return state;
-}
